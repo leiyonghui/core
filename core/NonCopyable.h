5ff7063e171b4c450276584aa4c57fd9207a3669
leiyonghui <729186631@qq.com>
2022-05-09 09:25:01 +0800

    fixed

--- core/NonCopyable.h
+++ core/NonCopyable.h
@@ -4,11 +4,11 @@ namespace core
 {
 	class CNoncopyable
 	{
-	public:
-		CNoncopyable(const CNoncopyable&) = delete;
-		CNoncopyable& operator=(const CNoncopyable&) = delete;
 	protected:
 		CNoncopyable() = default;
-		//~CNoncopyable() = default;
+		//virtual ~CNoncopyable() = default;
+
+		CNoncopyable(const CNoncopyable&) = delete;
+		CNoncopyable& operator=(const CNoncopyable&) = delete;
 	};
 }
