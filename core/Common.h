7f4e55b57dfc37c926a3046081f3689fbcb3c730
leiyonghui <729186631@qq.com>
2022-05-26 23:52:58 +0800

    #

--- core/Common.h
+++ core/Common.h
@@ -14,6 +14,17 @@ namespace core
 		return iter->second;
 	}
 
+	template<class Key, class Value, class Pr>
+	Value find(const std::unordered_map<Key, Value, Pr>& container, const Key& key, const Value& vaule) /*-> decltype(vaule)*/
+	{
+		auto iter = container.find(key);
+		if (iter == container.end())
+		{
+			return vaule;
+		}
+		return iter->second;
+	}
+
 	template<class K, class V>
 	bool insert(const std::map<K, V>& conatiner, const K& key, const V& value)
 	{
