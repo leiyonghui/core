a3e4e9c38044b40ac4aacb295415977cf0e66e02
leiyonghui <729186631@qq.com>
2022-06-12 19:10:11 +0800

    #

--- core/Configs.h
+++ core/Configs.h
@@ -19,14 +19,16 @@
 #include "Singleton.h"
 #include "Macro.h"
 
-using std::placeholders::_1;
-using std::placeholders::_2;
-using std::placeholders::_3;
-using std::placeholders::_4;
-using std::placeholders::_5;
-
 namespace core
 {
+	using std::placeholders::_1;
+	using std::placeholders::_2;
+	using std::placeholders::_3;
+	using std::placeholders::_4;
+	using std::placeholders::_5;
+
+	using string = std::string;
+
 	namespace types
 	{
 		typedef signed char int8;
